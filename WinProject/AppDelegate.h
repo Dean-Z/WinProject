e78893144bff0bdc3a7253d67b1c341581c8e86d
Dean-Z <deanjobsios@hotmail.com>
2014-04-17 12:15:08 +0800

    Initial Commit

---
+++ WinProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WinProject
+//
+//  Created by Dean on 14-4-17.
+//  Copyright (c) 2014年 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WinProjectTests/WinProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  WinProjectTests.m
+//  WinProjectTests
+//
+//  Created by Dean on 14-4-17.
+//  Copyright (c) 2014年 Dean. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WinProjectTests : XCTestCase
+
+@end
+
+@implementation WinProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
