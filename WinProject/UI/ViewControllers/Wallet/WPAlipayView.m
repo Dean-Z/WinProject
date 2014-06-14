8045d10332e53af5febd2613f4380d76519a1923
Dean-Z <deanjobsios@hotmail.com>
2014-06-15 00:00:10 +0800

    fix bug

--- WinProject/UI/ViewControllers/Wallet/WPAlipayView.m
+++ WinProject/UI/ViewControllers/Wallet/WPAlipayView.m
@@ -78,7 +78,8 @@
 {
     if (alipayView == nil)
     {
-        alipayViewContainer = [[UIScrollView alloc]initWithFrame:CGRectMake(self.sizeW, 44, self.sizeW, self.tableview.sizeH - 44)];
+        alipayViewContainer = [[UIScrollView alloc]initWithFrame:CGRectMake(self.sizeW, 44, self.sizeW, self.tableview.sizeH)];
+        alipayViewContainer.contentInset = UIEdgeInsetsMake(0, 0, 44, 0);
         alipayViewContainer.showsHorizontalScrollIndicator = NO;
         alipayViewContainer.showsVerticalScrollIndicator = NO;
         alipayViewContainer.backgroundColor = [UIColor clearColor];
